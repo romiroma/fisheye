27e3b6aa84313c38224667c23ef641d4958879d0
Roman Andrykevych <romiroma@icloud.com>
2022-07-17 14:22:07 +0300

    Initial Commit

---
+++ FishEye/FishEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishEyeApp.swift
+//  FishEye
+//
+//  Created by Roman on 17.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FishEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
